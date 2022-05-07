79fa9752218be72529e1af77b5fe945f25cb7cfc
Rahat47 <djrayhan8@gmail.com>
2022-05-08 01:18:29 +0600

    created missions slice, added mission type definition

---
+++ src/features/mission/missionSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { fetchMissions } from '../../api';
+import { RootState } from '../../app/store';
+import { Mission } from '../../types';
+
+const initialState: {
+    loading: boolean;
+    error: string | null;
+    data: Mission[] | null;
+} = {
+    loading: false,
+    error: null,
+    data: null,
+};
+
+export const missionSlice = createSlice({
+    name: 'mission',
+    initialState,
+    reducers: {
+        setMissions: (state, action: PayloadAction<Mission[]>) => {
+            state.data = action.payload;
+        },
+    },
+
+    extraReducers: builder => {
+        builder.addCase(fetchMissions.fulfilled, (state, action) => {
+            state.loading = false;
+            state.data = action.payload;
+            state.error = null;
+        });
+
+        builder.addCase(fetchMissions.rejected, (state, action) => {
+            state.loading = false;
+            state.error = 'Failed to fetch missions';
+        });
+
+        builder.addCase(fetchMissions.pending, state => {
+            state.loading = true;
+        });
+    },
+});
+
+export const { setMissions } = missionSlice.actions;
+export const selectMissions = (state: RootState) => state.mission.data;
+
+export default missionSlice.reducer;
