9bade59844ed02768b00b71b71fe3b748b51b3e4
Rahat47 <djrayhan8@gmail.com>
2022-05-08 01:17:36 +0600

    added the redux store and custom hooks for typescript usage

---
+++ src/app/store.ts
@@ -0,0 +1,17 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import missionReducer from '../features/mission/missionSlice';
+
+export const store = configureStore({
+    reducer: {
+        mission: missionReducer,
+    },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>;
